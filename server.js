a903ea831b162c036b3f56a4172455cddce05518
Adnan Shuja <39210333+adnanshuja@users.noreply.github.com>
2021-08-25 02:23:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const routes = require("./routes/index");
 const runMig = require("./initialize");
 const hostname = "127.0.0.1";
-const port = 3001;
+const port = 5000;
 
 const app = express();
 app.use(logger("dev"));
