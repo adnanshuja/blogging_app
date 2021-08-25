4bba35ea92e1e730b44480362af261a47f2ab888
Adnan Shuja <39210333+adnanshuja@users.noreply.github.com>
2021-08-25 23:14:01 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const routes = require("./routes/index");
 const runMig = require("./initialize");
 const hostname = "0.0.0.0";
 const port = 3001;
 
 const app = express();
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
